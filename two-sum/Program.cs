7ac329ba4d1c738e7674a71f9842bde96a867a9e
oykumm <62017320+oykumm@users.noreply.github.com>
2020-03-23 16:58:59 +0300

    Add files via upload

---
+++ two-sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace two_sum
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            int i, target;
+            int[] n = new int[10];
+            int[] v = new int[10];
+            for (i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine("Enter {0}. number ", i + 1);
+                n[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the target ={0}");
+                target = Convert.ToInt32(Console.ReadLine());
+                v = TwoSum(n, target);
+                Console.WriteLine("v ={0}",v);
+                Console.ReadKey();
+
+            }
+
+
+        }
+        public static int[] TwoSum(int[] numbers, int t)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (dict.ContainsKey(t - numbers[i])) // checks if compliment is in dict
+                {
+                    return new int[] { dict[t - numbers[i]], i };
+                }
+                else if (!dict.ContainsKey(numbers[i])) // handles duplicates in array
+                {
+                    dict.Add(numbers[i], i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
